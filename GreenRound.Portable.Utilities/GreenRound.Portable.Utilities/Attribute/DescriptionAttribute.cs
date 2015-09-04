272cc16741097a7f3ddf2be9445189b1b8839835
Kevin Giffone <kevin.giffone@gmail.com>
2015-09-04 16:24:56 +0200

    -Add DescriptionAttribute class

---
+++ GreenRound.Portable.Utilities/GreenRound.Portable.Utilities/Attribute/DescriptionAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+//TODO: Document and unit test
+namespace GreenRound.Portable.Utilities.Attribute
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class DescriptionAttribute : System.Attribute
+    {
+        private string _description;
+
+
+        public string Description
+        {
+            get { return _description; }
+        }
+
+        protected string DescriptionValue
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+
+        public DescriptionAttribute(string description)
+        {
+            _description = description;
+        }
+
+        public DescriptionAttribute() : this(String.Empty)
+        {
+            
+        }
+
+        public override bool Equals(object obj)
+        {
+            DescriptionAttribute descriptionAttribute = obj as DescriptionAttribute;
+            if (descriptionAttribute != null
+                && descriptionAttribute.Description == this.Description)
+            {
+                return true;
+            }
+            return false;
+        }       
+    }
+}
