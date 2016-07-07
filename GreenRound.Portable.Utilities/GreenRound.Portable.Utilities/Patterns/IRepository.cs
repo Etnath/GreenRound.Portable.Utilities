26e8affcc4d83f2adebf1b43e1c1fc9b2ce63171
Kevin Giffone <kevin.giffone@gmail.com>
2016-07-07 16:29:10 +0200

    Add repository and unit of work patterns (WIP)

---
+++ GreenRound.Portable.Utilities/GreenRound.Portable.Utilities/Patterns/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenRound.Portable.Utilities.Patterns
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        IEnumerable<T> GetEntities();
+    }
+}
---
+++ GreenRound.Portable.Utilities/GreenRound.Portable.Utilities/Patterns/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenRound.Portable.Utilities.Patterns
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+        void Rollback();
+    }
+}
